c1c5b10a027f594ab50480c9bf30980644062fba
open-meadow <rohan.varughese@protonmail.com>
2023-11-15 23:59:19 -0500

    save to localStorage

--- client/app.js
+++ client/app.js
@@ -1,4 +1,5 @@
 // When user clicks on Google Sign-In, it returns OAuth info
 function handleLogin (response) {
     console.log(response);
+    localStorage.setItem("token", response.credential);
 }
