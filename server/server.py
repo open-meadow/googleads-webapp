f5db4a57885a51e81952b2a17ac48e240f813b2d
open-meadow <rohan.varughese@protonmail.com>
2023-11-17 01:25:50 -0500

    Create client-secrets path

--- server/server.py
+++ server/server.py
@@ -1,13 +1,18 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, session
+from auth.auth import authorize
 
 _CLIENT_URL = "http://localhost:4200"
 
 app = Flask(__name__)
+app.secret_key = "SECRET KEY"
 
 # flow -- will go here first
 @app.route("/authorize")
 def authorize_endpoint():
-    url = "???"
+    auth_info = authorize()
+    passthrough_val = auth_info["passthrough_val"]
+    session["passthrough_val"] = passthrough_val
+    url = auth_info["authorization_url"]
     return redirect(url)
 
 # after generating refresh token, this will redirect back to client URL
