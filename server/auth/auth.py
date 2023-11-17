5179aa73ec1a8f09e952f8112ae05d1b9a619384
open-meadow <rohan.varughese@protonmail.com>
2023-11-17 00:41:07 -0500

    Create auth function

---
+++ server/auth/auth.py
@@ -0,0 +1,33 @@
+import hashlib
+import os
+
+# If using Web flow, the redirect URL must match exactly what’s configured in GCP for
+# the OAuth client.  If using Desktop flow, the redirect must be a localhost URL and
+# is not explicitly set in GCP.
+from google_auth_oauthlib.flow import Flow
+
+_CLIENT_SECRETS_PATH = os.environ["CLIENT_SECRETS_PATH"]
+_SCOPE = "https://www.googleapis.com/auth/adwords"
+_SERVER = "127.0.0.1"
+_PORT = 8080
+_REDIRECT_URI = f"http://{_SERVER}:{_PORT}/oauth2callback"
+
+# Generates URL and anti-forgery state token
+
+
+def authorize():
+    flow = Flow.from_client_secrets_file(_CLIENT_SECRETS_PATH, scopes=[_SCOPE])
+    flow.redirect_uri = _REDIRECT_URI
+
+    # Create an anti-forgery state token as described here:
+    # https://developers.google.com/identity/protocols/OpenIDConnect#createxsrftoken
+    passthrough_val = hashlib.sha256(os.urandom(1024)).hexdigest()
+
+    authorization_url, state = flow.authorization_url(
+        access_type="offline",
+        state=passthrough_val,
+        prompt="consent",
+        include_granted_scopes="true",
+    )
+
+    return {"authorization_url": authorization_url, "passthrough_val": passthrough_val}
--- server/server.py
+++ server/server.py
@@ -1,8 +1,16 @@
 from flask import Flask, redirect
 
+_CLIENT_URL = "http://localhost:4200"
+
 app = Flask(__name__)
 
+# flow -- will go here first
 @app.route("/authorize")
 def authorize_endpoint():
     url = "???"
     return redirect(url)
+
+# after generating refresh token, this will redirect back to client URL
+@app.route("/oauth2callback")
+def oauth2callback_endpoint():
+    return redirect(_CLIENT_URL)
