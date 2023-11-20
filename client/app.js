7533f5f940ce31c5c36f43181faa3f5dced33825
open-meadow <rohan.varughese@protonmail.com>
2023-11-20 01:16:59 -0500

    Safety commit

--- client/app.js
+++ client/app.js
@@ -1,5 +1,12 @@
+SERVER_URL = "http://127.0.0.1:5000"
+
 // When user clicks on Google Sign-In, it returns OAuth info
 function handleLogin (response) {
     console.log(response);
     localStorage.setItem("token", response.credential);
+}
+
+// redirect to GoogleAds Authentication
+function onLinkAdsAccount() {
+    window.location.href = `${SERVER_URL}/authorize`;
 }
